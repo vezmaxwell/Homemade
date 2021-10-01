b04ed89852026c0490300c154daff5c787415b2d
Vez Maxwell <vezmaxwell@gmail.com>
2021-10-01 14:56:41 +0100

    login/signup pages authenticated, ternery for navbar

--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+ 
 const SignUp = () => {
 
+    //* History
+    const history = useHistory()
+
+    //* State
+    const [ formData, setFormData ] = useState({
+      email: '',
+      username: '',
+      password: '',
+      passwordConfirmation: ''
+    })
+  
+    //* Functions 
+    const handleChange = (event) => {
+      const newObj = { ...formData, [event.target.name]: event.target.value }
+      setFormData(newObj)
+    }
+  
+    const setTokenToLocalStorage = (token) => {
+      window.localStorage.setItem('token', token)
+    }
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+      try {
+        const { data } = await axios.post('/api/register', formData)
+        setTokenToLocalStorage(data.token)
+        history.push('/')
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
   return (
     <>
@@ -9,39 +42,39 @@ const SignUp = () => {
       <div className="signUpPage">
 
       <div className="form-header sign-up-form-header">
-        <h2>Login to your account.</h2>
+        <h2>Sign up for an account.</h2>
 
-        <h3>Don't have an account? 
-          <a href="/">Get one here.</a> 
+        <h3>Already have an account? 
+          <a href="/login"> Login here.</a> 
         </h3> 
         
         </div>
 
         <div className="form-container sign-up-form-container">
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
 
           <div className="formfield">
             <p>Email</p>
-            <input type="text" placeholder="Your email goes here"/>
+            <input onInput={handleChange} type="email" name="email" value={formData.email} placeholder="Your email goes here"/>
           </div>
 
           <div className="formfield">
             <p>Username</p>
-            <input type="text" placeholder="Your username goes here"/>
+            <input onInput={handleChange} type="text" name="username" value={formData.username} placeholder="Your username goes here"/>
           </div>
         
           <div className="formfield">
             <p>Password</p>
-            <input type="text" placeholder="Your email goes here"/>
+            <input onInput={handleChange} type="text" name="password" value={formData.password} placeholder="Your email goes here"/>
           </div>
 
           <div className="formfield">
             <p>Password Confirmation</p>
-            <input type="text" placeholder="Confirm your password"/>
+            <input onInput={handleChange} type="text" name="passwordConfirmation" value={formData.passwordConfirmation} placeholder="Confirm your password"/>
           </div>
 
-          <button className="form-button">LOGIN</button>
+          <button className="form-button">SIGN UP</button>
 
         </form>    
         </div>
--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom'
 import logo from '../../images/fork.png'
+import { userIsAuthenticated } from '../helpers/auth'
 
 const Navbar = () => {
+
+  //* History
+  const history = useHistory()
+
+  //* Location
+  const location = useLocation()
+
+  useEffect(() => {
+// Triggers rerender with path change
+  }, [location.pathname])
+
+
+
+  //* We can put this in and the below handleLogout function, and make it so you can only see 'Add Recipe' or 'Logout' if the user is logged in
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('token')
+    history.push('/')
+  }
 
   return (
 
@@ -10,27 +30,41 @@ const Navbar = () => {
       <div className="container">
         <Link className="navLink" to='/'>
 
+{/* Logo Img */}
 
           <div className="logo-simble">
             <img src={logo} alt="" className="logo" />
           </div>
         </Link>
 
+{/* Logo Name */}
+
         <Link className="navLink" to='/'>
           <h1 className="homemade">Homemade</h1>
         </Link>
 
+{/* Navigation Links */}
+
         <div className="links">
+
           <ul>
 
-            <li><Link className="navLink" to='/Login'>Login</Link></li>
-            <li><Link className="navLink" to='/SignUp'>SignUp</Link></li>
-            <li><Link className="navLink" to='/SearchRecipe'>All Recipes</Link></li>
-          </ul>
-          <div className="search-bar">
-            <input type="text" placeholder="Search for something..." />
-          </div>
+        { 
+          userIsAuthenticated() ?
+            <>
+              <li className="navLink" onClick={handleLogout}>Logout</li>
+              <li><Link className="navLink" to='/'>Add Recipe</Link></li>
+              <li><Link className="navLink" to='/'>All Recipes</Link></li>
+            </>
+            :
+            <>
+              <li><Link className="navLink" to='/Login'>Login</Link></li>
+              <li><Link className="navLink" to='/SignUp'>SignUp</Link></li>
+              <li><Link className="navLink" to='/SearchRecipe'>All Recipes</Link></li>
+            </>
+          }
 
+          </ul>
         </div>
       </div>
     </div>
